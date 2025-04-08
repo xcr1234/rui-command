051f0dcc7ac3d0b285ead39b88f15d545ca0c8ed
xcr <530551426@qq.com>
2025-04-08 16:42:34 +0800

    init commit,hello

---
+++ awesome_bot/plugins/foo.py
@@ -0,0 +1,11 @@
+from nonebot import on_command
+from nonebot.adapters import Message
+from nonebot.params import CommandArg
+
+rui = on_command('rui',priority=10, block=True)
+
+@rui.handle()
+async def handle_function(args: Message = CommandArg()):
+    real_command = args.extract_plain_text()
+    if real_command == 'hello':
+        await rui.finish('hello')
---
+++ bot.py
@@ -0,0 +1,18 @@
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter
+from pathlib import Path
+
+# 初始化 NoneBot
+nonebot.init()
+
+# 注册适配器
+driver = nonebot.get_driver()
+driver.register_adapter(Adapter)
+
+# 在这里加载插件
+nonebot.load_builtin_plugins("echo")  # 内置插件
+nonebot.load_plugin(Path("./awesome_bot/plugins/foo.py"))  # 加载项目插件
+
+
+if __name__ == "__main__":
+    nonebot.run()
