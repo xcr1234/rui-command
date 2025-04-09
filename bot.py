60499481bbd5d60e3a2384f66de25b325cc2b3fc
xcr_1 <530551426@qq.com>
2025-04-09 21:32:44 +0800

    优化日志输出

--- bot.py
+++ bot.py
@@ -1,6 +1,24 @@
+import sys
+
 import nonebot
 from nonebot.adapters.onebot.v11 import Adapter
 from pathlib import Path
+from nonebot.log import default_format, default_filter, logger_id, logger
+
+
+def custom_filter(record):
+    return default_filter(record) and record['name'] != 'nonebot'
+
+# 移除 NoneBot 默认的日志处理器
+logger.remove(logger_id)
+# 添加新的日志处理器
+logger.add(
+    sys.stdout,
+    level=0,
+    diagnose=True,
+    format=default_format,
+    filter=custom_filter
+)
 
 # 初始化 NoneBot
 nonebot.init()
