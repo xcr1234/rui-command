1f4904874e58d38cecd9e4d345172f3c7c02c158
xcr_1 <530551426@qq.com>
2025-04-10 00:24:24 +0800

    增加tts

--- awesome_bot/plugins/voice_gen.py
+++ awesome_bot/plugins/voice_gen.py
@@ -40,9 +40,10 @@ def voice_gen_impl(text: str):
                 用户的输入是：{text}""")
     logging.info(f'{llm_model} emotion {emotion_text}')
 
-    response_text = call_llm(f""""你的名字叫锐锐，一名小学女生。请用一句话回复用户的输入，不要过长，不要输出多余内容，也不要输出标点符号。
+    response_text = call_llm(f""""你的名字叫锐锐，一名小学女生。请根据当前情绪，用一句话回复用户的输入，不要过长，不要输出多余内容，也不要输出标点符号。
     请注意：如果是政治或色情类敏感问题固定回答“这个问题不太合适吧”。
     
+    你的情绪是：{emotion_text if emotion_text in prompt_dict else '默认'}
     用户的输入是：{text}""")
 
     logging.info(f'{llm_model} response {response_text}')
