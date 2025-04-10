fb4c6dd9890f33f6181ecc4b7b8ede6dd362c2c7
xcr_1 <530551426@qq.com>
2025-04-11 01:04:21 +0800

    add mongo

--- awesome_bot/plugins/ruirui/mongo_store.py
+++ awesome_bot/plugins/ruirui/mongo_store.py
@@ -1,10 +1,11 @@
 import nonebot
 from pymongo import MongoClient
 from urllib.parse import quote_plus
+
 config = nonebot.get_driver().config
 
 
-MONGO_URL = f'mongodb://{quote_plus(config.mongodb_username)}:{quote_plus(config.mongodb_password)}@{config.mongodb_host}:{config.mongodb_port}/'
+MONGO_URL = f'mongodb://{quote_plus(config.mongodb_username)}:{quote_plus(config.mongodb_password)}@{config.mongodb_host}:{config.mongodb_port}/{config.database_name}?authSource={config.mongodb_auth_source}'
 
 
 def save_voice_log(data: dict):
