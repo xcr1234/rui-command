76e2907a303826f6cf676abdfe01030e69841e29
xcr_1 <530551426@qq.com>
2025-04-10 21:56:12 +0800

    尝试输出前面加一点静音

--- awesome_bot/plugins/voice_gen.py
+++ awesome_bot/plugins/voice_gen.py
@@ -197,7 +197,7 @@ async def handle_function(args: Message = CommandArg()):
             logging.info(f'temp_file {temp_file}')
             download_url_to_file(voice_url, temp_file)
             original_audio = AudioSegment.from_mp3(temp_file)
-            silence = AudioSegment.silent(duration=1100)
+            silence = AudioSegment.silent(duration=100)
             new_audio = silence + original_audio  # 前面加静音
             new_audio.export(temp_file, format="mp3")
             await voice.finish(MessageSegment.record(file=temp_file))
